652cfdb196eda23192361ff8ee8c5b85f789ac5b
Biswajit <biswajitmohapatra7377@gmail.com>
2025-10-26 22:51:41 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { ArrowUp } from "lucide-react";
 export const Footer = () => {
   return (
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
-      <p className="text-sm text-muted-foreground pl-20">
+      <p className="text-sm text-muted-foreground pl-10">
         &copy; {new Date().getFullYear()} Biswajit.co. All rights reserved.
       </p>
       <a
